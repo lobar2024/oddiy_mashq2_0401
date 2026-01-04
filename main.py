00e1a4b1eb5348f877fe58c2cc1d9252bf035de5
Lobar Farxodova <qodirovjavohir2109@gmail.com>
2026-01-04 23:38:36 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+numbers = [3, 7, 2, 9, 4, 10, 1]
+
+even_numbers = [n for n in numbers if n % 2 == 0]
+
+print("Juftlar:", even_numbers)
+print("Yig‘indi:", sum(even_numbers))
+print("Eng katta:", max(numbers))
+print("Eng kichik:", min(numbers))
